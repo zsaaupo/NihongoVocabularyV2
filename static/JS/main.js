f60cd30f62e138c2eb9a3feccfed162e93e8d202
zsaaupo <zakariasarker407@gmail.com>
2023-05-10 01:28:23 +0600

    N5 Voc ajax added

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,20 +1,38 @@
-$(document).ready(function() {
+$('table').hide()
 
-    
-    // ajax for N5 Voc list
+// ajax for N5 Voc
+function table(value){
+    $('#N5_list').hide()
+    $('table').show()
     $.ajax({
-      url: "/N5/list/",
-      type: "GET",
-      dataType: "json",
-      success: function(response) {
-          for (i in response){
-              $("#N5_list").append(`<br><br><br><button class="button-30" role="button">List ${response[i].list_number}</button>`);
-          };
-      },
-      error: function(xhr, status, error) {
+        url: value,
+        type: "GET",
+        dataType: "json",
+        success: function(response) {
+          for(i in response){
+            $("#voc_table").append(`<tr><td><button class="button-30" role="button">${response[i].nihongo}</button></td>
+            <td><button class="button-30" role="button">${response[i].english}</button></td>
+            <td><button class="button-30" role="button">${response[i].bangla}</button></td><tr>`)
+          }
+        },
+        error: function(xhr, status, error) {
+          console.log(xhr.responseText);
+        }
+      });
+};
+
+
+// ajax for N5 Voc list
+$.ajax({
+    url: "/N5/list/",
+    type: "GET",
+    dataType: "json",
+    success: function(response) {
+        for (i in response){
+            $("#N5_list").append(`<br><br><br><a><button onclick="table('/N5/list/${response[i].list_number}')" class="button-30" role="button">List ${response[i].list_number}</button></a>`);
+        };
+    },
+    error: function(xhr, status, error) {
         console.log(xhr.responseText);
-      }
-    });
-
-
+    }
 });
