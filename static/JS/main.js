babe2304f14cbe46e845d79b73a43c3ab096798f
zsaaupo <zakariasarker407@gmail.com>
2023-05-09 22:24:37 +0600

    ajax added for N5 voc list

---
+++ static/JS/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $.ajax({
+      url: "/N5/list/",
+      type: "GET",
+      dataType: "json",
+      success: function(response) {
+          for (i in response){
+              $("#N5_list").append(`<br><br><br><button class="button-30" role="button">List ${response[i].list_number}</button>`);
+          };
+      },
+      error: function(xhr, status, error) {
+        console.log(xhr.responseText);
+      }
+    });
+  });
