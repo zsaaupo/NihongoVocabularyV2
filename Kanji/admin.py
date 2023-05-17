97e95e3fccfd949939a1fb8c17211f23b86e86ba
zsaaupo <zakariasarker407@gmail.com>
2023-05-17 14:48:35 +0600

    kanji model registered

--- Kanji/admin.py
+++ Kanji/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class KanjiN5Admin(admin.ModelAdmin):
+    
+    fields = [
+        "kanji",
+        "nihongo"
+    ]
+    
+admin.site.register(KanjiN5, KanjiN5Admin)
+
+
+class KanjiN4Admin(admin.ModelAdmin):
+    
+    fields = [
+        "kanji",
+        "nihongo"
+    ]
+    
+admin.site.register(KanjiN4, KanjiN4Admin)
--- Kanji/models.py
+++ Kanji/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from N5.models import VocabularyN5
+from N4.models import VocabularyN4
 
-# Create your models here.
+class KanjiN5(models.Model):
+    
+    nihongo = models.ForeignKey(VocabularyN5, on_delete=models.PROTECT, related_name="vocabulary")
+    kanji = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.kanji + " " + self.nihongo
+    
+
+class KanjiN4(models.Model):
+    
+    nihongo = models.ForeignKey(VocabularyN4, on_delete=models.PROTECT, related_name="vocabulary")
+    kanji = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.kanji + " " + self.nihongo
+    
